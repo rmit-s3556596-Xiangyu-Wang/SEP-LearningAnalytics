b726b7348add4c9539131f752ce42410596a9487
Nebojsa-Pajkic <30699693+Nebojsa-Pajkic@users.noreply.github.com>
2017-08-30 14:39:43 +1000

    Login page

---
+++ SEP/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>RMIT - Learning Analytics</title>
+<link rel="stylesheet" href="css.css" type="text/css" />
+</head>
+<body>
+<h1>RMIT - Learning Analytics</h1>
+</body>
+</html>
