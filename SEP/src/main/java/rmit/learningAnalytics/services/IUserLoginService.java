542228b0d8a0fd8c35d671e40f867d3649e4c631
Nebojsa-Pajkic <30699693+Nebojsa-Pajkic@users.noreply.github.com>
2017-08-30 11:36:26 +1000

    Delete IUserLoginService.java

--- SEP/src/main/java/rmit/learningAnalytics/services/IUserLoginService.java
+++
@@ -1,8 +0,0 @@
-package rmit.learningAnalytics.services;
-
-import rmit.learningAnalytics.entites.User;
-
-public interface IUserLoginService {
-//	boolean findByUsername(String username, String password);
-	User getUserByName(String name);
-}
