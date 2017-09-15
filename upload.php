fb897d3541f40156c641692a92b31994e198190a
Nebs <s3054047@student.rmit.edu.au>
2017-09-15 18:09:23 +1000

    css changes

--- upload.php
+++ upload.php
@@ -153,57 +153,110 @@
 <body>
 	<header> RMIT - Learning Analytics </header>
 
-
-
-	
-
-
 	<main>
-	
+
 	<div class="upload_facility">
 		<input type="file" id="fileUpload" /> <input type="button"
 			class="upload" id="upload" value="Upload" />
 	</div>
 
-	<div class="nav">
-	<input type="checkbox" id="c3" name="xxx" onclick="onToggle(this);" value="3"/>Academic Career <br/><br/>
-    <input type="checkbox" id="c4" name="xxx" onclick="onToggle(this);" value="4"/>Acad Level(By Term) <br/><br/>
-    <input type="checkbox" id="c5" name="xxx" onclick="onToggle(this);" value="5"/>Program Campus <br/><br/>
-    <input type="checkbox" id="c6" name="xxx" onclick="onToggle(this);" value="6"/>Term <br/><br/>
-    <input type="checkbox" id="c7" name="xxx" onclick="onToggle(this);" value="7"/>Program Code <br/><br/>
-    <input type="checkbox" id="c8" name="xxx" onclick="onToggle(this);" value="8"/>Academic Plan <br/><br/>
-    <input type="checkbox" id="c9" name="xxx" onclick="onToggle(this);" value="9"/>Admit Term <br/><br/>
-    <input type="checkbox" id="c10" name="xxx" onclick="onToggle(this);" value="10"/>Course Owner <br/><br/>
-    <input type="checkbox" id="c11" name="xxx" onclick="onToggle(this);" value="11"/>Course Id <br/><br/>
-    <input type="checkbox" id="c12" name="xxx" onclick="onToggle(this);" value="12"/>Course Description <br/><br/>
-    <input type="checkbox" id="c13" name="xxx" onclick="onToggle(this);" value="13"/>Subject Id <br/><br/>
-    <input type="checkbox" id="c14" name="xxx" onclick="onToggle(this);" value="14"/>Catalogue Number <br/><br/>
-    <input type="checkbox" id="c15" name="xxx" onclick="onToggle(this);" value="15"/>Class Number <br/><br/>
-    <input type="checkbox" id="c16" name="xxx" onclick="onToggle(this);" value="16"/>Session Code <br/><br/>
-    <input type="checkbox" id="c17" name="xxx" onclick="onToggle(this);" value="17"/>Section Code <br/><br/>
-    <input type="checkbox" id="c18" name="xxx" onclick="onToggle(this);" value="18"/>Class Campus <br/><br/>
-    <input type="checkbox" id="c19" name="xxx" onclick="onToggle(this);" value="19"/>Grade Entered <br/><br/>
-    <input type="checkbox" id="c20" name="xxx" onclick="onToggle(this);" value="20"/>Grade Official <br/><br/>
-    <input type="checkbox" id="c21" name="xxx" onclick="onToggle(this);" value="21"/>Grade Roster Input <br/><br/>
-    <input type="checkbox" id="c22" name="xxx" onclick="onToggle(this);" value="22"/>Grade In Roster Status <br/><br/>
-    <input type="checkbox" id="c23" name="xxx" onclick="onToggle(this);" value="23"/>Term GPA <br/><br/>
-    <input type="checkbox" id="c24" name="xxx" onclick="onToggle(this);" value="24"/>Program GPA <br/><br/>
-    <input type="checkbox" id="c25" name="xxx" onclick="onToggle(this);" value="25"/>Total Units Attempt <br/><br/>
-    <input type="checkbox" id="c26" name="xxx" onclick="onToggle(this);" value="26"/>Total Units Pass <br/><br/>
-    <input type="checkbox" id="c27" name="xxx" onclick="onToggle(this);" value="27"/>Funding Source <br/><br/>
-    <input type="checkbox" id="c28" name="xxx" onclick="onToggle(this);" value="28"/>TACADPR Comments <br/><br/>
-    <input type="checkbox" id="c29" name="xxx" onclick="onToggle(this);" value="29"/>Total Units Credit <br/><br/>
-    <input type="checkbox" id="c30" name="xxx" onclick="onToggle(this);" value="30"/>Cumulative Units <br/><br/>
-    <input type="checkbox" id="c31" name="xxx" onclick="onToggle(this);" value="31"/>Student Email Address
-	</div>
+	<table width="100%" class="checkboxes">
 
+		<tr>
+			<td><input type="checkbox" id="c3" name="xxx"
+				onclick="onToggle(this);" value="3" />Academic Career</td>
+			<td><input type="checkbox" id="c13" name="xxx"
+				onclick="onToggle(this);" value="13" />Subject Id</td>
+			<td><input type="checkbox" id="c23" name="xxx"
+				onclick="onToggle(this);" value="23" />Term GPA</td>
+		</tr>
+
+		<tr>
+			<td><input type="checkbox" id="c4" name="xxx"
+				onclick="onToggle(this);" value="4" />Acad Level (by Term)</td>
+			<td><input type="checkbox" id="c14" name="xxx"
+				onclick="onToggle(this);" value="14" />Catalogue Number</td>
+			<td><input type="checkbox" id="c24" name="xxx"
+				onclick="onToggle(this);" value="24" />Program GPA</td>
+		</tr>
+
+		<tr>
+			<td><input type="checkbox" id="c5" name="xxx"
+				onclick="onToggle(this);" value="5" />Program Campus</td>
+			<td><input type="checkbox" id="c15" name="xxx"
+				onclick="onToggle(this);" value="15" />Class Number</td>
+			<td><input type="checkbox" id="c25" name="xxx"
+				onclick="onToggle(this);" value="25" />Total Units Attempt</td>
+		</tr>
+
+		<tr>
+			<td><input type="checkbox" id="c6" name="xxx"
+				onclick="onToggle(this);" value="6" />Term</td>
+			<td><input type="checkbox" id="c16" name="xxx"
+				onclick="onToggle(this);" value="16" />Session Code</td>
+			<td><input type="checkbox" id="c26" name="xxx"
+				onclick="onToggle(this);" value="26" />Total Units Passed</td>
+		</tr>
+
+		<tr>
+			<td><input type="checkbox" id="c7" name="xxx"
+				onclick="onToggle(this);" value="7" />Program Code</td>
+			<td><input type="checkbox" id="c17" name="xxx"
+				onclick="onToggle(this);" value="17" />Section Code</td>
+			<td><input type="checkbox" id="c27" name="xxx"
+				onclick="onToggle(this);" value="27" />Funding Source</td>
+		</tr>
+
+		<tr>
+			<td><input type="checkbox" id="c8" name="xxx"
+				onclick="onToggle(this);" value="8" />Academic Plan</td>
+			<td><input type="checkbox" id="c18" name="xxx"
+				onclick="onToggle(this);" value="18" />Class Campus</td>
+			<td><input type="checkbox" id="c28" name="xxx"
+				onclick="onToggle(this);" value="28" />ACADPR Comments</td>
+		</tr>
+
+		<tr>
+			<td><input type="checkbox" id="c9" name="xxx"
+				onclick="onToggle(this);" value="9" />Admit Term</td>
+			<td><input type="checkbox" id="c19" name="xxx"
+				onclick="onToggle(this);" value="19" />Grade Entered</td>
+			<td><input type="checkbox" id="c29" name="xxx"
+				onclick="onToggle(this);" value="29" />Total Units Credit</td>
+		</tr>
+
+		<tr>
+			<td><input type="checkbox" id="c10" name="xxx"
+				onclick="onToggle(this);" value="10" />Course Owner</td>
+			<td><input type="checkbox" id="c20" name="xxx"
+				onclick="onToggle(this);" value="20" />Grade Official</td>
+			<td><input type="checkbox" id="c30" name="xxx"
+				onclick="onToggle(this);" value="30" />Cumulative Units</td>
+		</tr>
+
+		<tr>
+			<td><input type="checkbox" id="c11" name="xxx"
+				onclick="onToggle(this);" value="11" />Course Id</td>
+			<td><input type="checkbox" id="c21" name="xxx"
+				onclick="onToggle(this);" value="21" />Grade Roster Input</td>
+			<td><input type="checkbox" id="c31" name="xxx"
+				onclick="onToggle(this);" value="31" />Student Email Address</td>
+		</tr>
+
+		<tr>
+			<td><input type="checkbox" id="c12" name="xxx"
+				onclick="onToggle(this);" value="12" />Course Description</td>
+			<td><input type="checkbox" id="c22" name="xxx"
+				onclick="onToggle(this);" value="22" />Grade in Roster Status</td>
+		</tr>
+
+	</table>
+
+<div style="overflow-x:auto;">
 	<span class="section"></div>
+	</div>
 	
 	</main>
-
-	<footer>
-		<?php include("footer.php"); ?>
-	</footer>
 
 </body>
 </html>
