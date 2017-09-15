420de0fb83df628099a646ff77b0013f4ed2f9fd
Nebs <s3054047@student.rmit.edu.au>
2017-09-15 19:12:26 +1000

    changes to email text field

--- index.php
+++ index.php
@@ -14,12 +14,12 @@
 	<div class="login">
 		<form>
 			<p><label for="username">Staff email:</label></p>
-			<input type="text" name="username" id="username" autofocus autocomplete="on" placeholder="@rmit.edu.au">
+			<input type="email" name="username" id="username" autofocus autocomplete="on" placeholder="@rmit.edu.au" required title="RMIT email address">
 
 			<p><label for="password">Password:</label></p>
-			<input type="password" name="password" id="password">
+			<input type="password" name="password" id="password" required>
 			
-			<p class="center"><input type="reset" value="Clear">&nbsp<input type="submit" value="Submit"></p>
+			<p class="center"><input type="reset" value="Clear">&nbsp<input type="submit" value="Submit" title="Your password"></p>
 
 		</form>
 	</div>
