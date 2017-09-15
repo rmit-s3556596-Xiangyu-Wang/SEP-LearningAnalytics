42cbef6fc4cb0f166a3ff5c2d02ce40ce2688015
Nebs <s3054047@student.rmit.edu.au>
2017-09-15 20:52:04 +1000

    email validation script implemented

--- index.php
+++ index.php
@@ -4,6 +4,22 @@
 <meta charset="UTF-8">
 <title>RMIT - Learning Analytics</title>
 <link rel="stylesheet" href="css.css" type="text/css" />
+<script>
+	function checkEmail() {
+		"use strict";
+		var input;
+		input = document.getElementById("email").value;
+		if (input.endsWith('@rmit.edu.au')) {
+			console.log('Email validated.');
+			document.getElementById("Warning").innerHTML = "";
+			return true;
+		} else {
+			console.log("Incorrect email entered.");
+			document.getElementById("Warning").innerHTML = "Email address must end with <em>@rmit.edu.au</em>";
+			return false;
+		}
+	}
+</script>
 </head>
 <body>
 
@@ -12,16 +28,28 @@
 	<main>
 	<p>Login using your RMIT email and password.</p>
 	<div class="login">
-		<form>
-			<p><label for="username">Staff email:</label></p>
-			<input type="email" name="username" id="username" autofocus autocomplete="on" placeholder="@rmit.edu.au" required title="RMIT email address">
+		<form action="upload.php" method="post"
+			onsubmit="return checkEmail();>
+			<p>
+			
+			<label for="username">Staff email:</label>
+			</p>
+			<input type="email" name="email" id="email" autofocus
+				autocomplete="on" placeholder="@rmit.edu.au" required
+				title="RMIT email address">
 			<p id="warning">&nbsp</p>
 
-			<p><label for="password">Password:</label></p>
-			<input type="password" name="password" id="password" required title="Your password">
+			<p>
+				<label for="password">Password:</label>
+			</p>
+			<input type="password" name="password" id="password" required
+				title="Your password">
 			<p id="warning">&nbsp</p>
-			
-			<p class="center"><input type="reset" value="Clear">&nbsp<input type="submit" value="Submit"></p>
+
+			<p class="center">
+				<input type="reset" value="Clear">&nbsp<input type="submit"
+					value="Submit" onclick="checkEmail()">
+			</p>
 
 		</form>
 	</div>
