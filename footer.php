e40bc5d2119ddeea3ff9a7b6fe48144c5337a50c
Nebs <s3054047@student.rmit.edu.au>
2017-10-18 12:00:36 +1100

    added help page, included in footer, changed footer text

--- footer.php
+++ footer.php
@@ -1,2 +1,12 @@
-<p>RMIT - COSC2616 Software Engineering Postgraduate Project - 2017, Sem 2<br>
-&copy; 2017 <a href="mailto:nebojsa.pajkic@rmit.edu.au">Learning Analytics Team</a></p>
+<p>
+	<a href="courseanalysis.php">Course Analysis</a> | <a href="help.php">Help
+		Page</a> | <a href="index.php">Home Page</a> | <a
+		href="programanalysis.php">Program Analysis</a><br /> RMIT - COSC2616
+	Software Engineering Postgraduate Project - 2017, Semester 2<br />
+	&copy; 2017 <a href="mailto:s3054047@student.rmit.edu.au">Nebojsa
+		Pajkic</a>, <a href="mailto:s3578752@student.rmit.edu.au">Dongchen Li</a>,
+	<a href="mailto:s3556596@student.rmit.edu.au">Xiangyu Wang</a>, <a
+		href="mailto:s3592928@student.rmit.edu.au">Xiyan Huang</a>, <a
+		href="mailto:s3569255@student.rmit.edu.au">Keyu Chen</a>, &amp; <a
+		href="mailto:s3501037@student.rmit.edu.au">Bo Wang</a>
+</p>
---
+++ help.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php include("header.php"); ?> - Help Page</title>
+<link rel="stylesheet" href="css.css" type="text/css" />
+</head>
+<body>
+	<div class="container">
+		<header><?php include("header.php"); ?> - Help Page</header>
+
+		<section class="content">
+			<div class="main">
+				
+
+
+
+
+
+
+			</div>
+		</section>
+		<footer>
+		<?php include("footer.php"); ?>
+		</footer>
+	</div>
+</body>
+</html>
