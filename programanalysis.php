330a10a12ce1040c98f0e154bf0c2b035d786cf6
Nebs <s3054047@student.rmit.edu.au>
2017-10-20 13:14:43 +1100

    bug with 'block'

--- programanalysis.php
+++ programanalysis.php
@@ -859,7 +859,7 @@
             if (state == 'none') {
                 document.getElementById(id).style.display = 'block';
             } else {
-                document.getElementById(id).style.display = 'none;
+                document.getElementById(id).style.display = 'none';
             }
         }
     </script>
