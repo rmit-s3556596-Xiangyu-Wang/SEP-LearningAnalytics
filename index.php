d1a87fad43743b1ca008b5447e332086dcf44735
shaun <s3556596@student.rmit.edu.au>
2017-10-26 19:36:56 +1100

    add browser detect

--- index.php
+++ index.php
@@ -1,44 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title><?php include("header.php"); ?></title>
-<link rel="stylesheet" href="css.css" type="text/css" />
-<script>
-        function iFrameHeight() {
-            var ifm= document.getElementById("iframeset");
-            var subWeb = document.frames ? document.frames["iframeset"].document : ifm.contentDocument;
-            if(ifm != null && subWeb != null) {
-                ifm.height = subWeb.body.scrollHeight;
-                //ifm.width = 1425;
-            }
-        }
-    </script>
-<!--    <script>-->
-<!--        var OSName="Unknown OS";-->
-<!--        if (navigator.appVersion.indexOf("Win")!=-1) {-->
-<!--            OSName = "Windows";-->
-<!--            document.getElementById("program").attributes("href", "./programanalysis.php");-->
-<!--            alert(document.getElementById("program").attributes);-->
-<!--        }-->
-<!--        if (navigator.appVersion.indexOf("Mac")!=-1){-->
-<!--            OSName="MacOS";-->
-<!--            document.getElementById("course").attributes("href", "./programanalysis_mac.php");-->
-<!--        }-->
-<!--        if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";-->
-<!--        if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";-->
-<!--    </script>-->
-
-<!--    <script type="text/javascript">-->
-<!--        var isMac = (navigator.platform.indexOf("MacPPC") != -1) ? true : false;-->
-<!--        if(isMac) location.href = "index_mac.php";-->
-<!--        else location.href = "index.php";-->
-<!--    </script>-->
+    <title><?php include("header.php"); ?></title>
+    <link rel="stylesheet" href="css.css" type="text/css" />
     <script>
         var OSName="Unknown OS";
         if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
         if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
         if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
         if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+    </script>
+    <script type="text/javascript">
+        var isChrome = navigator.userAgent.toLowerCase().match(/chrome/) != null;
+        var message = "*We highly recommend you to use Google Chrome to get best experience.";
+    </script>
+    <script>
+
     </script>
     <script>
         function navigatorProgram() {
@@ -61,21 +38,30 @@
     </script>
 </head>
 <body>
-	<div class="container">
-		<header><?php include("header.php"); ?></header>
+<div class="container">
+    <header><?php include("header.php"); ?></header>
+    <section class="content">
+        <div class="main">
+            <p id="bversion" style="text-align: center">
+                <script type="text/javascript">
+                    if (!isChrome) {
+                        document.write(message);
+                        document.write("<br/>");
+                    }
+                </script>
+            </p>
+            <div class="middle">
 
-		<section class="content">
-			<div class="main">
-				<div class="middle">
-                    <h1>
-                        Enter<a href="javascript:navigatorProgram();">Program Analysis</a> page or <a href="javascript:navigatorCourse();">Course Analysis</a> page
-                    </h1>
-				</div>
-			</div>
-		</section>
-		<footer>
-		<?php include("footer.php"); ?>
-		</footer>
-	</div>
+                <h1>
+                Enter <a href="javascript:navigatorProgram();">Program Analysis</a> page or <a href="javascript:navigatorCourse();">Course Analysis</a> page
+            </h1>
+            </div>
+
+        </div>
+    </section>
+    <footer>
+        <?php include("footer.php"); ?>
+    </footer>
+</div>
 </body>
 </html>
